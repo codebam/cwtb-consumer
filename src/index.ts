d98e7cef033e460fcbef16f26c0ad434bd1b41ab
Sean Behan <codebam@riseup.net>
2024-05-15 04:10:22 -0400

    add telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import TelegramBot, { TelegramExecutionContext } from '@codebam/cf-workers-telegram-bot';
+
+interface Environment {
+	TOKEN: string;
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request, env: Environment, ctx: ExecutionContext): Promise<Response> {
+		const bot = new TelegramBot(env.TOKEN);
+		bot.on('start', async function (context: TelegramExecutionContext) {
+			switch (context.update_type) {
+				case 'message':
+					await context.reply('Hello World');
+					break;
+
+				default:
+					break;
+			}
+			return new Response('ok');
+		});
 		return new Response('Hello World!');
 	},
 };
